04575d1c08f8e7205c56716e529776d5d784595b
lizhenhu <2740128806@qq.com>
2018-03-21 21:22:52 +0800

    add all

---
+++ js/holepage.js
@@ -0,0 +1,141 @@
+function fullpage(cb, animateEnd){
+			var el = $('#container');
+			var section = $('.section');
+			var wh = window.innerHeight;
+			var sy;
+			var my;
+			var ry;
+			var active;
+			var n;
+			var p;
+			var canswipe = true;
+
+			function pullDown(){
+				
+				active.css({
+					transformOrigin: 'center bottom',
+					transition: 'transform ' + .4 * (wh - ry) / wh + 's ease-in',
+					transform: 'scale(.5)'
+				}).removeClass('active');
+				p.css({
+					transition: 'transform ' + .4 * (wh - ry) / wh + 's ease-in',
+					zIndex: 2,
+					transform: 'scale(1) translate(0,0)'
+				}).addClass('active');
+			
+			}
+
+			function pullUp(){
+				
+				active.css({
+					transformOrigin: 'center top',
+					transition: 'transform ' + .4 * (wh - ry) / wh + 's ease-in',
+					transform: 'scale(.5)'
+				}).removeClass('active');
+				n.css({
+					transition: 'transform ' + .4 * (wh - ry) / wh + 's ease-in',
+					zIndex: 2,
+					transform: 'scale(1)'
+				}).addClass('active');
+			
+			}
+
+			$(document).on('touchstart', function(e){
+
+                e.preventDefault();
+
+				if(!canswipe){
+					return;
+				}
+
+				sy = e.targetTouches[0].clientY;
+				active = $('.section.active');
+				n = active.next();
+				p = active.prev();
+				section.each(function(){
+					$(this).css({
+						transition: '0s',
+						zIndex: 0
+					})
+				});
+				active.css({
+					zIndex: 1
+				});
+				p.css({
+					transform: 'translate(0,-100%)'
+				});
+				n.css({
+					transform: 'translate(0,100%)'
+				});
+
+                $(document).on('touchmove', function(e){
+
+                    e.preventDefault();
+
+					my = e.targetTouches[0].clientY;
+					ry = my - sy;
+					var scale;
+					if(ry>0 && p.length){
+						scale = 1 - ry / wh / 2;
+						active.css({
+							transform: 'scale(' + scale + ')',
+							transformOrigin: 'center bottom',
+						});
+						n.css({
+							transform: 'translate(0,100%)'
+						});
+						p.css({
+							transform: 'translate(0, ' + (ry - wh) + 'px)',
+							zIndex: 2
+						});
+					}
+					else if(ry<=0 && n.length){
+						scale = (1 + ry/wh/2) <.5? .5 : (1 + ry/wh/2);
+						active.css({
+							transform: 'scale(' + scale + ')',
+							transformOrigin: 'center top',
+						});
+						n.css({
+							transform: 'translate(0, ' + (wh + ry) + 'px)',
+							zIndex: 2
+						});
+						p.css({
+							transform: 'translate(0,-100%)'
+						});
+					}
+
+				});
+
+                $(document).on('touchend', function(e){
+
+                    e.preventDefault();
+
+                    $(document).off('touchmove');
+                    $(document).off('touchend');
+
+					canswipe = false;
+
+					if(ry>0 && p.length){
+						pullDown();
+					}
+					else if(ry<=0 && n.length){
+						pullUp();
+					}
+                    var $el = $('.section.active');
+                    cb && setTimeout(function(){
+						cb.call($el, $el, section.index($el));
+					},300);
+					setTimeout(function(){
+						canswipe = true;
+						animateEnd && animateEnd($el, $el, section.index($el));
+					},1000);
+
+				});
+
+			});
+
+			;(function () {
+				var el = $('.section.active');
+				cb && cb.call(el, el, section.index(el));
+			})()
+		}
